4bf538d639d291c3a59e788b21eb3b5f0c9fde99
ConnorSchill <125571387+ConnorSchill@users.noreply.github.com>
2023-02-16 07:20:24 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,40 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index <= 2; index++) {
+        music.playTone(262, music.beat(BeatFraction.Quarter))
+        basic.showNumber(3 - index)
+    }
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    basic.showString("GO!")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . # . # .
+        . . . . .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Do you want to see my new racing stripe?")
+    basic.clearScreen()
+    basic.pause(5000)
+    basic.showString("I will show it.")
+    basic.clearScreen()
+    basic.pause(500)
+    basic.showLeds(`
+        . # # # .
+        . # # # .
+        . # # # .
+        . # # # .
+        . # # # .
+        `)
+    basic.pause(5000)
+    basic.clearScreen()
+})
+loops.everyInterval(60000, function () {
+    music.playMelody("F G F G A B C5 D ", 500)
+})
+basic.forever(function () {
+	
+})
